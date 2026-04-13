f7e340c02ceaacf0447530e672c5b9710dc91ccb
alsi-dot <lizhaynes6@gmail.com>
2026-04-13 14:10:29 -0500

    simple detect(untested)

--- backend/blob_detect.py
+++ backend/blob_detect.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage import feature, color, io
+#grab modified photo through out_path
 from photo_modification import out_path
-
+#set up image and detector
 image = out_path()
-
 detector = cv2.SimpleBlobDetector_create()
-
+#detect blobs
 blobs = detector.detect(image)
+#
+image_blobs = cv2.drawKeypoints(image, *blobs, color=(255, 0, 0))
 
+cv2.imshow("blobs found",image_blobs)
+cv2.waitKey(0)
 
-image_blobs = cv2.drawKeypoints()
