6dfa8ef1ae902ef4d6cbb5c6789d586d82ed928c
alsi-dot <lizhaynes6@gmail.com>
2026-04-13 14:35:55 -0500

    Add parameters to be adjusted

--- backend/blob_detect.py
+++ backend/blob_detect.py
@@ -7,9 +7,12 @@ image = out_path()
 detector = cv2.SimpleBlobDetector_create()
 #detect blobs
 blobs = detector.detect(image)
-#
+
 image_blobs = cv2.drawKeypoints(image, *blobs, color=(255, 0, 0))
 
 cv2.imshow("blobs found",image_blobs)
 cv2.waitKey(0)
+#parameters
+parms = cv2.SimpleBlobDetector_Params()
+
 
