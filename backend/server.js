f52374b8e21206589fa995e4c4396737a17029ae
Shorez-11 <zshore04@gmail.com>
2026-04-15 16:00:06 -0500

    Revert "Jade/redo websocket backend websocket"

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const socket = new WebSocket('ws://localhost:8080');
-
-socket.addEventListener('open', function (event) {
-  console.log('WebSocket connection established');
-});
-
-socket.addEventListener('message', function (event) {
-  console.log('Message from server ', event.data);
-});
-
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Backend' });
 });
